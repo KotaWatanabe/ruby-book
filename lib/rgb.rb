5fe3c5ddbe6c8051d38e739f4815027846864cf8
Kota Watanabe <kota3316@gmail.com>
2022-02-06 16:08:53 -0800

    convert_hash_tag

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,12 +1,23 @@
+# def to_hex(r,g,b)
+#     [r,g,b].inject('#') do |hex,n|
+#         hex += n.to_s(16).rjust(2,'0')
+#     end
+# end
+
+# def to_ints(hex)
+#     r,g,b = hex[1..2],hex[3..4],hex[5..6]
+#     [r,g,b].map do |s|
+#         s.hex
+#     end
+# end
+
 def to_hex(r,g,b)
-    [r,g,b].inject('#') do |hex,n|
-        hex += n.to_s(16).rjust(2,'0')
-    end
+    [r,g,b].inject('#'){|hex, n| hex += n.to_s(16).rjust(2,'0')}
 end
 
 def to_ints(hex)
-    r,g,b = hex[1..2],hex[3..4],hex[5..6]
-    [r,g,b].map do |s|
-        s.hex
-    end
+    r = hex[1..2]
+    g = hex[3..4]
+    b = hex[5..6]
+    [r,g,b].map(&:hex)
 end
